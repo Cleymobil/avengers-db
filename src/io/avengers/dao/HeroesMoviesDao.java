e0bf91fef93a4a0894d1810ba033b8533524f40b
CBaty <camille.defroberville@gmail.com>
2017-03-15 11:00:10 +0100

    heroesMoviesDao prepared statement

--- src/io/avengers/dao/HeroesMoviesDao.java
+++ src/io/avengers/dao/HeroesMoviesDao.java
@@ -1,6 +1,7 @@
 package io.avengers.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,13 +14,14 @@ public class HeroesMoviesDao extends MarvelDao {
 
 	public Set<String> findMoviesByHeroesname(String term) throws SQLException {
 
-		String query = "SELECT m.name FROM movie_hero mh JOIN heroes h ON mh.id_hero=h.id JOIN movie m ON m.id=mh.id_movie WHERE h.name LIKE '%"+term+"%' ORDER BY m.date; ";
+		String query = "SELECT m.name FROM movie_hero mh JOIN heroes h ON mh.id_hero=h.id JOIN movie m ON m.id=mh.id_movie WHERE h.name LIKE '%?%' ORDER BY m.date; ";
 
 		// port 3306, no password
 		Connection connect = connectToMySql();
 
-		Statement statement = connect.createStatement();
-		ResultSet resultSet = statement.executeQuery(query);
+		PreparedStatement statement = connect.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
+		statement.setString(1, term);
+		ResultSet resultSet = statement.getGeneratedKeys();
 
 		Set<String> moviesHeroes = new HashSet<>();
 
