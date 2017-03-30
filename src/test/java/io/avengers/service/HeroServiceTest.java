df05b27682a3b72a703f4742bcb14c7c3be53fe2
saramencor <sara.mendez.cortinas@gmail.com>
2017-03-30 13:36:13 +0200

    test service hero dao

--- src/test/java/io/avengers/service/HeroServiceTest.java
+++ src/test/java/io/avengers/service/HeroServiceTest.java
@@ -7,6 +7,8 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import io.avengers.domain.Hero;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -14,7 +16,7 @@ public class HeroServiceTest {
 
 	HeroService heroService;
 
-	
+	Hero test = new Hero(0,"tonton1");
 	
 	@Before
 	public void setUp() throws Exception {
@@ -31,11 +33,24 @@ public class HeroServiceTest {
 		assertTrue(heroService.findAll().size()>1);
 		assertFalse(heroService.findAll().size()<0);
 	}
-	@Ignore
+	
 	@Test
 	public void testFindHeroesByName(){
 		assertTrue(heroService.findHeroesByName("Spider").size()==1);
-		;
+		assertTrue((heroService.findHeroesByName("Spider").iterator().next()).getName().contains("i"));
+		
 	}
-
+	@Test
+	public void testFindHeroesById() throws SQLException{
+		assertTrue(heroService.findHeroesById(1).getName().equals("Spiderman"));
+		assertFalse(heroService.findHeroesById(1).getName().equals("Hulk"));
+	}
+	@Test
+	public void testNewHero() throws SQLException {
+		Hero hero=heroService.createHero(test);
+		hero.setIrl("tururu");
+		assertTrue(hero.getId()!=0);
+		heroService.deleteHero(hero);
+		
+	}
 }
