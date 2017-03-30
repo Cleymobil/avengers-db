adbe4ac434703939af54aeea6a8b6772dc60f7c4
cleymobil <clement.coutey@hotmail.fr>
2017-03-30 11:58:36 +0200

    TeamTest

--- src/main/java/io/avengers/ui/TestApplicationTeam.java
+++ src/main/java/io/avengers/ui/TestApplicationTeam.java
@@ -1,7 +1,9 @@
 package io.avengers.ui;
 
+import java.sql.SQLException;
 import java.util.Set;
 
+import io.avengers.dao.TeamDao;
 import io.avengers.domain.Team;
 import io.avengers.service.TeamService;
 
@@ -9,7 +11,7 @@ public class TestApplicationTeam {
 
 	public static void main(String[] args) {
 
-		
+		TeamDao dao=new TeamDao();
 		TeamService service = new TeamService();
 		 //System.out.println(service.findAll());
 
@@ -19,7 +21,10 @@ public class TestApplicationTeam {
 		 System.out.println(t.getName());
 		 
 		 
-		 Set<Team> teams= service.findTeamsByName("men");
+		
+		
+		 
+		 Set<Team> teams= service.findTeamsByName("Av");
 		 System.out.println(teams);
 		
 		 Team te= service.findTeamByName("Avengers");
